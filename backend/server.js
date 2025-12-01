00e5a0954fcd8f6e71e8e858ae1a98a2c12a1bae
OlivierBrun82 <contactpro@olivierbrun.eu>
2025-12-01 14:24:40 +0100

    websocket

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,42 @@
-// crée le serveur express
-const { log } = require("console");
-const { Socket } = require("dgram");
+// importation du module express
 const express = require("express");
-// crée le serveur http
+// importation du module http
 const http = require("http");
-// crée le server socket.io
+// importation du module socket.io
 const { Server } = require('socket.io');
 
-// création de l'instance express
+// création de l'instance express pour le server http
 const app = express();
 
-// 
+// création du server http pour le server socket.io
 const server = http.createServer(app);
 
+// création de l'instance socket.io pour le server socket.io
 const io = new Server(server, {
-    cors:{
+    cors: {
         origin: "http://localhost:5173",
-        methods: ["GET","POST"]
+        methods: ["GET", "POST"],
+        credentials: true,
+        allowedHeaders: ["*"]
     }
 });
 
 
 io.on("connection", (socket) => {
+    // console.log("l'utilisateur vient de se connecter", socket.id)
+    socket.on("bouton", (data) => {
+        console.log(data, socket.id);
+        // envoi du message à tous les clients
+       socket.broadcast.emit("bouton_reponse", data);
 
-    console.log("l'utilisateur vient de se connecter", socket.id)
+
+    });
+    
+    // fonction pour recevoir un message du client
+    socket.on("message", (data) => {
+        console.log(data)
+        socket.broadcast.emit("message_reponse", data)
+    });
     
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,73 @@
 import './App.css'
 import { io } from "socket.io-client";
+import { useEffect } from 'react';
+import { useState } from 'react';
 
+// connexion au serveur socket.io
 const socket = io("http://localhost:3000");
 
 
 
 function App() {
 
-console.log(socket.id);
+  // message envoyé
+  const [message, setMessage] = useState('');
+  // tout les messages reçus
+  const [messages, setMessages] = useState([]);
 
+// fonction pour envoyer un message au serveur
+  function btnClick(){
+    socket.emit("bouton", 'Hello World');
+  }
+
+  // fonction pour recevoir un message du serveur
+  useEffect(() => {
+    socket.on("bouton_reponse", (data) => {
+      console.log(data);
+    });
+    socket.on("message_reponse", (data) => {
+      setMessages((prev) => [...prev, "Serveur: " + data]);
+    })
+
+    // fonction pour déconnecter le socket
+    return () => {
+      socket.off("bouton_reponse");
+      socket.off("message_reponse");
+    };
+
+  }, []);
+
+// console.log(socket.id);
+
+  function envoyer(){
+    if (message.trim === '') {
+      return;
+    }
+    // envoi du message au serveur pour moi
+    setMessages((prev) => [...prev, "Moi: " + message]);
+
+    socket.emit("message", message)
+  };
 
   return (
     <>
       <div>
           <h1>Exo Websocket</h1>
-      </div>
+          <div><button onClick={btnClick}>Test</button></div>
+        <input type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        />
+        <button onClick={envoyer}>Envoyer</button>
 
+        <div>
+          {messages.map((message, index) => (
+            <div key={index}>{message}</div>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
